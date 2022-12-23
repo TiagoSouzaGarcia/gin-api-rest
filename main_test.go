de2d2d261c2b42fda6ec225e07b9df4fa74197c7
Tiago Garcia <tiago.garcia@asq.com.br>
2022-12-23 11:13:15 -0300

    Test with testify

--- main_test.go
+++ main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/TiagoSouzaGarcia/api-go-gin/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine {
@@ -18,14 +21,23 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	r := SetupDasRotasDeTeste()
 	r.GET("/:nome", controllers.Saudacao)
 	//Requisicao que será passada
-	req, _ := http.NewRequest("GET", "/tiago", nil)
+	req, _ := http.NewRequest("GET", "/Tiago", nil)
 	//Resposta que será gravada com NewRecorder(). implementa a interface de response writer.
 	resposta := httptest.NewRecorder()
 	//Realiza a requisição. Tem como parametros a resposta da requisição e a requisição
 	r.ServeHTTP(resposta, req)
 
 	//Validacao do resultado
-	if resposta.Code != http.StatusOK {
+	/* if resposta.Code != http.StatusOK {
 		t.Fatalf("Status error: valor recebido foi %d e o esperado era %d", resposta.Code, http.StatusOK)
-	}
+	} */
+
+	//Verificando o status code da resposta
+	assert.Equal(t, http.StatusOK, resposta.Code, "Status error: valor recebido foi %d e o esperado era %d", resposta.Code, http.StatusOK)
+	//Verificando o conteúdo da resposta
+	mockDaResposta := `{"API diz:":"E ai Tiago, tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+	fmt.Println(string(respostaBody))
+	fmt.Println(mockDaResposta)
 }
